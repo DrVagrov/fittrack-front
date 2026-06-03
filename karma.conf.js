8d027c96eea20a8f55bd553687db87846b7ccaa2
DrVagrov <72596408+DrVagrov@users.noreply.github.com>
2026-06-03 10:01:10 +0200

    add matrix

--- karma.conf.js
+++ karma.conf.js
@@ -1,45 +1,17 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/6.4/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
-    basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fittrack-front'),
-      subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }]
-    },
-    reporters: ['progress', 'kjhtml'],
     browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-gpu',
-          '--headless',
-          '--remote-debugging-port=9222'
-        ]
+        flags: ['--no-sandbox']
       }
-    },
-    captureTimeout: 120000,
-    restartOnFileChange: true
+    }
   });
 };
